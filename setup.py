c0672d02bd1c3e6ed1fbbea53f5e2faca389432b
Justin Mohr <jmohr@bytepulse.net>
2011-08-03 10:05:51 -0600

    fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,15 @@ setup(
     author_email = 'jmohr@bytepulse.net',
     packages = find_packages(exclude=['*.test.*', '*.test']),
     version = '1.2',
-    install_requires = install_requires
+    install_requires = install_requires,
     license = 'BSD',
     url = 'http://github.com/jmohr/compago',
     package_data = {'':['README']},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development',
+    ]
 )
